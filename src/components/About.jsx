8bb7bf2d2122450bf290e0b10bcd1afd6ee26e03
caitroach <roachc006@gmail.com>
2026-05-12 18:33:48 -0400

    plz work

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,10 @@ const BIO = `hi, i'm cait!! bio here!`;
 
 const LINKS = [
   { label: "github", href: "https://github.com/caitroach" },
-  { label: "are.na", href: "https://are.na/cait" },
+  { label: "the gram", href: "https://instagram.com/cait.42" },
+  { label: "fable", href: "https://fable.co/fabler/cait-594836290790"}
   { label: "letterboxd", href: "https://letterboxd.com/cait42" },
+  { label: "spotify", href: "https://open.spotify.com/user/pkzv78wyv7jntbq6nt56minc9"}
   { label: "email", href: "roachc006@gmail.com" },
 ];
 
