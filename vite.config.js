2dcd49887bc9aa3e7ae89264dbf21e5dbfc2d2a7
caitroach <roachc006@gmail.com>
2026-05-18 21:07:58 -0400

    i cant reed :3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Markdown from 'vite-plugin-markdown';
+import Markdown from 'vite-plugin-react-markdown';
 
 export default defineConfig({
   plugins: [react(), Markdown()],
