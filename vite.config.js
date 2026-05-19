0c79f30e39038314fe410acb25403be4612ee910
caitroach <roachc006@gmail.com>
2026-05-18 21:09:25 -0400

    ok so i dont need to add vite plugin markdown... maybe... hold on

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Markdown from 'vite-plugin-react-markdown';
 
 export default defineConfig({
   plugins: [react(), Markdown()],
