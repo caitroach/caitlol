d6ce656f4a3f7619cf2c5372d9963406cd496157
caitroach <roachc006@gmail.com>
2026-05-18 21:10:21 -0400

    im actually idiot

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), Markdown()],
+  plugins: [react()],
   base: "/",
   build: {
     outDir: "docs",
